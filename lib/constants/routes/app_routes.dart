ebd226cc5f668b89a8d9f30069a277ebce9838c7
Abdallah-Abdalmonem <bedo30176@gmail.com>
2023-10-14 23:40:13 +0300

    Refactor

--- lib/constants/routes/app_routes.dart
+++ lib/constants/routes/app_routes.dart
@@ -2,9 +2,6 @@ import 'package:application/presentation/nineteen_screen/nineteen_screen.dart';
 import 'package:application/presentation/twenty_screen/twenty_screen.dart';
 import 'package:flutter/material.dart';
 
-import '../../presentation/nineteen_screen/test.dart';
-import '../../presentation/twenty_screen/test.dart';
-
 class AppRoutes {
   static const String nineteenScreen = '/nineteen_screen';
 
@@ -13,7 +10,5 @@ class AppRoutes {
   static Map<String, WidgetBuilder> routes = {
     nineteenScreen: (context) => NineteenScreen(),
     twentyScreen: (context) => TwentyScreen(),
-    // nineteenScreen: (context) => NineteenScreen(),
-    // twentyScreen: (context) => TwentyScreen(),
   };
 }
